07dca3e11bd9d51dec827c95a8af6c23f45fef8f
l3elk4 <119819542+l3elk4@users.noreply.github.com>
2023-05-01 23:34:52 +0300

    feat: add notation and basic page layout

--- virtual-keyboard/index.js
+++ virtual-keyboard/index.js
@@ -1,1 +1,10 @@
 //Begin
+
+const body = document.querySelector('body');
+const notation = document.createElement('p');
+notation.setAttribute('class', 'notation');
+
+notation.innerText = 'This keyboard was proudly made in the MacOs\n'
+    + 'For language change press leftCmd + leftAlt';
+
+body.appendChild(notation);
